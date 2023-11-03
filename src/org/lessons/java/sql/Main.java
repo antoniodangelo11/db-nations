ef93d3129eb1aba372159b0ccabd4359e88191a8
Antonio D'Angelo <reapercsp@gmail.com>
2023-11-03 14:50:31 +0100

    db connected

--- src/org/lessons/java/sql/Main.java
+++ src/org/lessons/java/sql/Main.java
@@ -1,7 +1,26 @@
 package org.lessons.java.sql;
 
-public class Main {
-    public static void main(String[] args) {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        // Connessione
+        String url = "jdbc:mysql://localhost:3306/db_nations";
+        String user = "root";
+        String password = "root";
+
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(con != null){
+                con.close();
+            }
+        }
     }
 }
