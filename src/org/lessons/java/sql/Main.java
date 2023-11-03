0864a7bdbd36ef906dcb563eb1cc79d5c261c49e
Antonio D'Angelo <reapercsp@gmail.com>
2023-11-03 17:12:15 +0100

    add search

--- src/org/lessons/java/sql/Main.java
+++ src/org/lessons/java/sql/Main.java
@@ -13,24 +13,36 @@ public class Main {
 
         try { Connection con = DriverManager.getConnection(url, user, password);
 
+            // Inizializzo lo Scanner
+            Scanner input = new Scanner(System.in);
+            System.out.println("Fai un ricerca: ");
+            String searchString = input.nextLine();
+
             String query = "SELECT c.country_id, c.name, r.name, c2.name "
                     + "FROM countries c "
                     + "JOIN regions r ON r.region_id = c.region_id "
                     + "JOIN continents c2 ON c2.continent_id = r.continent_id "
-                    + "ORDER BY c.name";
+                    + "WHERE c.name LIKE ? "
+                    + "ORDER BY c.name ";
 
             try (PreparedStatement preparedStatement = con.prepareStatement(query)) {
-                    // Qui possono andare i parametri
+
+                // Qui metto i parametri di ricerca
+                preparedStatement.setString(1, "%" + searchString + "%");
+
                 try (ResultSet resultSet = preparedStatement.executeQuery()) {
 
                     while (resultSet.next()) {
-
-                        int countryId = resultSet.getInt("country_id");
-                        String countryName = resultSet.getString("c.name");
-                        String regionName = resultSet.getString("r.name");
+                        int countryId        = resultSet.getInt("country_id");
+                        String countryName   = resultSet.getString("c.name");
+                        String regionName    = resultSet.getString("r.name");
                         String continentName = resultSet.getString("c2.name");
 
-                        System.out.println(countryId + " " + countryName + " " + regionName + " " + continentName);
+                        System.out.println(
+                                countryId + " - "
+                                + countryName + " - "
+                                + regionName + " - "
+                                + continentName);
                     }
                 } catch (SQLException e) {
                     System.out.println("Unable to execute query");
